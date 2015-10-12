5a6f651f65c79625f74e385a232bdab293fbfb36
Hadiya Hope-Crichlow <hopecrichlow@live.com>
2015-10-12 01:31:20 -0400

    Content HTML and JS added

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 (function () {
 
-  console.log('It Works!');
+ var firstItem = etsy.results[0];
+
+  var templateString = $('#itemTemplate').text();
+
+  var templateFunction = _.template(templateString);
+
+  _.each(etsy.results, function (item) {
+    var itemHTML = templateFunction(item);
+    $('.graphics').append(itemHTML);
+
+  });
+
 
 }());
